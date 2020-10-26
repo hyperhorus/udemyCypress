ba79e217f17315ba45c6bb3afb44d0c774d969f8
Guillermo Llano <hyperhorus@gmail.com>
2020-10-26 11:48:40 -0600

    Trabajando con alias para operaciones UI

--- cypress/integration/examples/table.spec.js
+++ cypress/integration/examples/table.spec.js
@@ -2,16 +2,26 @@
 
 describe("Testing EA App", () =>{
     
+    
     it('Login operation', () =>{
 
         cy.visit("http://eaapp.somee.com/");
         
         //cy.contains("Login").click();
-        cy.get("#loginLink").then(($link) => {
-            const linkText = $link.text();
-            expect(linkText).is.eql('Login');
-        }).click();
+        // Método largo de hacer un alias
+        // cy.get("#loginLink").then(($link) => {
+        //     return $link.text();
+        // }).as("linkText");
+        //Metodo corto de hacer un alias
+        cy.get("#loginLink").invoke('text').as("linkText");
 
+        cy.contains("Login").click();
+
+        cy.get("@linkText").then(($x) => {
+            expect($x).is.eql('Login');
+        })
+
+        
         cy.url().should("include", "/Account/Login");
 
         cy.get('#UserName').type("admin");
@@ -23,7 +33,15 @@ describe("Testing EA App", () =>{
         //Table
         //cy.get('.table').find('tr').filter('td'); una manera
         //cy.get('.table').find('tr > td'); otra manera de encontrar los elemtos dentro de un tr
-        cy.get('.table').find('tr > td').contains('Prashanth').parent().contains('Benefits').click();
+        //cy.get('.table').find('tr').contains('Prashanth').parent().contains('Benefits').click();
+
+        cy.get('.table').find('tr').as("rows");
+
+        cy.get("@rows").then(($row) =>{
+            cy.wrap($row).click({multiple:true});
+        })
+
+
 
 
 
