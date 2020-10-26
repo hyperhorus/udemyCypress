1254b7acc66c145a1401b77587508b7bbae20c29
Guillermo Llano <hyperhorus@gmail.com>
2020-10-26 09:17:13 -0600

    Agregando archivos del curso Cypress

---
+++ cypress/integration/examples/ea.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe("Testing of EA App", () =>{
+
+    it("Login Application", () =>{
+
+        cy.visit("http://eaapp.somee.com/");
+        
+        cy.contains("Login").click();
+
+        cy.url().should("include", "/Account/Login");
+
+        cy.get('#UserName').type("admin");
+        cy.get('#Password').type("password");
+        cy.get('.btn').click();
+    })
+})
+
+
---
+++ cypress/integration/examples/table.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+describe("Testing EA App", () =>{
+    
+    it('Login operation', () =>{
+
+        cy.visit("http://eaapp.somee.com/");
+        
+        cy.contains("Login").click();
+
+        cy.url().should("include", "/Account/Login");
+
+        cy.get('#UserName').type("admin");
+        cy.get('#Password').type("password");
+        cy.get('.btn').click({force:true});
+
+        cy.contains('Employee List').click();
+
+        //Table
+        //cy.get('.table').find('tr').filter('td'); una manera
+        //cy.get('.table').find('tr > td'); otra manera de encontrar los elemtos dentro de un tr
+        cy.get('.table').find('tr > td').contains('Prashanth').parent().contains('Benefits').click();
+
+
+
+    })
+})
